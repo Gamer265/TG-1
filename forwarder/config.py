30109317c9c16ecd7266239aee2f512e358e1e6e
Gamer265 <89581107+Gamer265@users.noreply.github.com>
2023-09-05 20:22:16 +0530

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -7,10 +7,10 @@ class Development(Config):
 
     # Make sure to include the '-' sign in group and channel ids.
     
-    FROM_CHATS = [-1001241675729,-1001881627229,-1001860656056,-1001877280212,-1001502705198,-1001749139811,-1001688333632,-1001797256809]# List of chat id's to forward messages from.
+    FROM_CHATS = [-1001792784756,-1001366147158,-1001169241764,-1001721377011,-1001241675729,-1001881627229,-1001860656056,-1001877280212,-1001502705198,-1001749139811,-1001688333632,-1001797256809]# List of chat id's to forward messages from.
     TO_CHATS = [-1001556839569]# List of chat id's to forward messages to.
 
     REMOVE_TAG = False
-    WORKERS = 16
+    WORKERS = 32
    
     
